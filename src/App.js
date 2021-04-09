816428cca2de0f5eccaad5a6b29d95b4a3f85d50
pratik mane <pratik.mane1299@gmail.com>
2021-04-09 22:30:23 +0530

    :sparkles: Add nav componet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Nav from './components/Nav';
+
 function App() {
   return (
-    <div className="p-6 flex justify-center items-center">
-      <span className="text-2xl">Tailwind css example</span>
+    <div className="w-full">
+      <Nav />
     </div>
   );
 }
