dbc18c2adf6e388364a974b97e0c65c50703aea0
pratik mane <pratik.mane1299@gmail.com>
2021-04-10 17:40:49 +0530

    ✨ Implement infinite scroll pagination

--- src/API.js
+++ src/API.js
@@ -1,11 +1,11 @@
 const API_URL = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABY_API}&image_type=photo&safesearch=true`;
 
-export function getImages(query) {
+export function getImages(query, page) {
   let url = ''
   if (query === '') {
-    url = API_URL;
+    url = `${API_URL}&page=${page}`;
   } else {
-    url = `${API_URL}&q=${query}`
+    url = `${API_URL}&q=${query}&page=${page}`;
   }
 
   return fetch(url)
--- src/App.js
+++ src/App.js
@@ -7,20 +7,39 @@ import { getImages} from './API';
 function App() {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    getImages(query)
-      .then((response) => setImages(response.hits));
-  }, [query]);
+    getImages(query, page)
+      .then((response) => {
+        const temp = [...images, ...response.hits];
+        setImages(temp);
+      });
+  }, [query, page]);
 
   const handleOnSearch = (term) => {
     setQuery(term)
   };
 
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    setPage(page+1);
+  }
+
   return (
     <div className="w-full">
       <Nav searchText={query} onSearch={handleOnSearch} />
       <ImageGrid images={images} />
+      {page < 26 ? (
+        <div className="flex justify-center py-4">
+          <button
+            className="py-2 px-4 rounded-3xl bg-purple-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+            onClick={handleOnClick}
+          >
+            Load More
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
