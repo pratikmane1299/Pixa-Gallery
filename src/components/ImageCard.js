a4853f1a9f16bb02345cd5b15a60018134850f76
pratik mane <pratik.mane1299@gmail.com>
2021-04-11 18:38:59 +0530

    💄Make responsive

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,16 +3,16 @@ import Tag from './Tag';
 function ImageCard({ image }) {
   return (
     <div
-      className="max-w-sm mx-auto bg-white rounded-lg shadow-md flex flex-col"
+      className="max-w-sm h-30 sm:w-46 sm:h-46 mx-auto bg-white rounded-lg shadow-md flex flex-col"
     >
-      <div className="w-full h-64 rounded-lg">
+      <div className="w-full h-full sm:h-64 rounded-lg">
         <img
           className="w-full h-full rounded-lg"
           alt={image.type}
           src={image.webformatURL}
         />
       </div>
-      <div className="w-full px-4 py-4 flex flex-col space-y-4">
+      <div className="hidden w-full px-4 py-4 lg:flex lg:flex-col lg:space-y-4">
         <h3 className="text-2xl text-center text-blue-500 cursor-pointer hover:underline">
           <a href={`https://pixabay.com/users/${image.user}/`}>{image.user}</a>
         </h3>
--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -2,7 +2,7 @@ import ImageCard from "./ImageCard";
 
 function ImageGrid({ images }) {
   return (
-    <div className="container mx-auto py-4">
+    <div className="px-4 lg:container lg:mx-auto py-4">
       <div className="grid grid-cols-3 gap-4">
         {images.map((image) => (
           <ImageCard key={image.id} image={image} />
